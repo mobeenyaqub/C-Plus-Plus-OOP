cdb855acbee297c0668de2bdac8b1143c8375a2c
Mubeen Yaqub <70191073+mobeenyaqub@users.noreply.github.com>
2021-02-24 13:39:00 +0500

    Add files via upload

---
+++ Encryption-Decryption.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int position{ 0 };
+	string name{};
+	char content{};
+	string alphabets{ "*ABCD.EFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789! @#$%^&()-_=+-/,<>?;{}[]|:'\n'" };
+	string key{ " .:|][}{;?><,-+=_-/)(&^%$#@!9876543210XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr*'\n'" };
+	cout << "Enter name of the file to encrypt : ";
+	cin >> name;
+	name += ".txt";
+
+	ifstream in_file;
+	in_file.open(name);
+	ofstream out_file{ "text.txt" };
+
+	if (!in_file) {
+		cerr << "\nUnable to open file. Try again!" << endl;
+	}
+	else {
+		cout << "Text file has been encrypted! New encrypted file created" << endl << endl;
+	}
+
+	while (in_file.get(content)) {
+
+		for (int i = 0; i < alphabets.length(); i++) {
+			if (content == alphabets[i]) {
+				position = i;
+
+				break;
+			}
+		}
+
+		content = key[position];
+		out_file.put(content);
+		position = 0;
+	}
+
+	in_file.close();
+	out_file.close();
+
+	ifstream i_file;
+	i_file.open("text.txt");
+	ofstream o_file{ "decrypter.txt" };
+
+	if (!i_file) {
+		cerr << "\nUnable to open text.txt file. Try again!" << endl;
+	}
+	else {
+		cout << "Read file has been opened!" << endl << endl;
+	}
+
+	if (!o_file) {
+		cerr << "\nUnable to open decrypted.txt file. Try again!" << endl;
+	}
+	else {
+		cout << "Write file has been opened!" << endl << endl;
+	}
+
+	char c{};
+	while (i_file.get(c)) {
+		for (int i = 0; i < alphabets.length(); i++) {
+			if (c == key[i]) {
+				position = i;
+				break;
+			}
+		}
+
+		c = alphabets[position];
+		o_file.put(c);
+		cout << c;
+		position = 0;
+	}
+
+	o_file.close();
+	i_file.close();
+	cout << endl << endl;
+
+	return 0;
+}
+
+
